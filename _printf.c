9da45a6b14674d7b18e7f1153a5c8c8261074aca
Olajumoke Fajuyi <olajumokefajuyi0@gmail.com>
2023-04-26 03:28:34 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,61 +1,19 @@
 #include "main.h"
 
 /**
- * _printf - produces output according to a format
- * @format: character string
- * @...: var
+ * _printf - produce output
+ * @format: character
  * Return: number of characters printed
  */
 
 int _printf(const char *format, ...)
 {
-	check_match chk[] = {
-		{"%s", putstrng}, {"%%", putper}, {"%b", putbin}, {"%c", print_char}, {"%d", putint}, {"%i", putint}
-	};
+	int num = 0;
+	va_list val;
 
-	va_list args;
-	int x;
-	int index;
-	int sizeof_ch;
-	int length;
+	va_start(val, format);
+	num = num + process_format(format, p, &val);
+	va_end(val);
 
-	va_start(args, format);
-	sizeof_ch = sizeof(chk) / sizeof(chk[0]);
-	length = 0;
-	x = 0;
-	index = 0;
-
-	if (format == NULL)
-		return (-1);
-	while (format[x] != '\0')
-	{
-		if (format[x] == '%' && format[x + 1] == '\0')
-			return (-1);
-		if (format[x] == '%')
-		{
-			while (index < sizeof_ch)
-			{
-				if (format[x + 1] == *(chk[index].id + 1))
-				{
-					length = length + chk[index].func(args);
-					x = x + 1;;
-					break;
-				}
-			}
-			index++;
-			if (index == sizeof_ch)
-			{
-				_putchar(format[x]);
-				length++;
-			}
-		}
-		else
-		{
-			_putchar(*format);
-			length++;
-		}
-	}
-	x++;
-	va_end(args);
-	return (length);
+	return (num);
 }
