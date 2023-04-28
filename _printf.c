7ef297108382c798e3eabd3da565d7524b8425e6
Olajumoke Fajuyi <olajumokefajuyi0@gmail.com>
2023-04-28 04:06:24 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -8,12 +8,67 @@
 
 int _printf(const char *format, ...)
 {
-	int num = 0;
+	int num = 0, len = 0;
 	va_list val;
 
 	va_start(val, format);
-	num = num + process_format(format, p, &val);
+	
+	if (format == NULL)
+		return (-1);
+	while (format[num])
+	{
+		if (format[num] == '%')
+		{
+			num++;
+			len = len + format_handler(format[num], val);
+		}
+		else
+			len = len + _putchar(format[num]);
+		num++;
+	}
+
 	va_end(val);
 
-	return (num);
+	return (len);
+}
+
+/**
+ * format_handler - to handle format directives
+ * @format: format character
+ * @val: va_list argument
+ * Return: number of character to be printed
+ */
+
+int format_handler(char format, va_list val)
+{
+	int len = 0;
+	int op = 1;
+
+	if (format == 'c')
+		len = len + _putchar(va_arg(val, int);
+	else if (format == 'x')
+		len = len + print_hex3(op, va_arg(val, unsigned int), 0);
+	else if (format == 'X')
+		len = len + print_hex3(op, va_arg(val, unsigned int), 1);
+	else if (format == 'o')
+		len = len + print_unsi(op, va_arg(val, unsigned int), 8);
+	else if (format == 's')
+		len = len + print_str(va_arg(val, char *));
+	else if (format == 'S')
+		len = len + print_S(va_arg(val, char *));
+	else if (format == '%')
+		len = len + _putchar('%');
+	else if (format == 'd')
+		len = len + print_number(va_arg(val, int));
+	else if (format == 'i')
+		len = len + print_number(va_arg(val, int));
+	else if (format == 'b')
+		len = len + print_bin(va_arg(val, unsigned long int));
+	else if (format == 'p')
+		len = len + print_pointer(va_arg(val, void *));
+	else if (format == 'u')
+		len = len + print_unsig(va_arg(val, unsigned int), 10);
+	else
+		len = len + _putchar('%'), len = len + _putchar(format);
+	return (len);
 }
--- main.h
+++ main.h
@@ -5,29 +5,22 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
+#define BUF_SIZE 1024
+#define ZERO_FLAG 4
+#define SPACE_FLAG 2
+#define PLUS_FLAG 1
 
 int _printf(const char *format, ...);
 int _putchar(char c);
 void _puts(char *str);
-
-/**
- * struct checker - for printf
- * @id: pointer
- * @func: pointer to function
- */
-
-typedef struct checker
-{
-	char *id;
-	int (*func)();
-} check_match;
-int putstrng(va_list list);
-int putper(void);
-int print_char(va_list arg);
-int putint(va_list list);
-int putni(va_list list, int print_sign);
-int putbin(va_list val);
-int print_rev(va_list val);
-int print_rot13(va_list val);
+int print_sig(unsigned int n, int base);
+int print_bin(unsigned long int n);
+int print_number(int n, int flags);
+int format_handler(char format, va_list va);
+int print_S(char *str);
+int print_hex3(int op, unsigned int n, int uppercase);
+int print_pointer(void *ptr);
+int print_str(char *str);
+int print_hex(int op, unsigned int n, int uppercase);
 
 #endif
